18714030e63badcb9a665e46f96997ea5ccfaee9
Ezekiel Inalegwu <ezekiel.inalegwu@commscope.com>
2022-11-05 21:59:29 -0600

    ch02: Added unit tests

--- app/src/test/java/com/akojimsg/msff/AppTest.java
+++ app/src/test/java/com/akojimsg/msff/AppTest.java
@@ -1,38 +1,15 @@
 package com.akojimsg.msff;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
+    @Test
+    public void testApp() {
+        Exception exception = Assertions.assertThrows(Exception.class, () -> {
+            App.main(null);
+        });
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+        Assertions.assertTrue(exception.getMessage().contains("org.xml.sax.SAXParseException"));
     }
 }
