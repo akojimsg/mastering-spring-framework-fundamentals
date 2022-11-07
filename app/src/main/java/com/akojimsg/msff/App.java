8c9dcfe2d46f64852dc35856ac7bc4b847225674
Ezekiel Inalegwu <ezekiel.inalegwu@commscope.com>
2022-11-06 20:34:31 -0600

    ch07 - clean app template

--- app/src/main/java/com/akojimsg/msff/App.java
+++ app/src/main/java/com/akojimsg/msff/App.java
@@ -1,22 +1,9 @@
 package com.akojimsg.msff;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Simple spring application
- *
- */
 public class App 
 {
     public static void main( String[] args ) {
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("application-context.xml");
-
-        MyService service = ctx.getBean(MyService.class);
-        service.doBussinessLogic();
-
-        MyRepository repository = ctx.getBean(MyRepository.class);
-        repository.doQuery();
-
+        System.out.println("Clean spring application template");
     }
 }
